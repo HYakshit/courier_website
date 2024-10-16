92de780721949dbb0407418c937f126cc7371fb4
akshi <akshit628325@gmail.com>
2024-10-17 00:38:22 +0530

    ship now added

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+document.onclick = function () {
+
+    $('.navbar-collapse').collapse('hide');
+
+};
+$('.js-scroll-trigger').click(function () {
+
+    $('.navbar-collapse').collapse('hide');
+});
